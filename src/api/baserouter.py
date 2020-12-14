b51f2d661c9f94973c8f94310417715cde0bc323
solnsumei <solnsumei@gmail.com>
2020-12-14 04:54:53 +0100

    Add response model to routes for proper documentation

--- src/api/baserouter.py
+++ src/api/baserouter.py
@@ -4,21 +4,21 @@ from fastapi import APIRouter
 def base_crud_router(request_schema, response_schema, model):
     router = APIRouter()
 
-    @router.get("/")
+    @router.get("/", response_model=list[response_schema])
     async def fetch_all():
         return await response_schema.from_queryset(model.all())
 
-    @router.get("/{item_id}")
+    @router.get("/{item_id}", response_model=response_schema)
     async def fetch_one(item_id: int):
         return await response_schema \
             .from_queryset_single(model.get(id=item_id))
 
-    @router.post("/", status_code=201)
+    @router.post("/", status_code=201, response_model=response_schema)
     async def create(item: request_schema):
         new_item = await model.create_one(item)
         return await response_schema.from_tortoise_orm(new_item)
 
-    @router.put("/{item_id}")
+    @router.put("/{item_id}", response_model=response_schema)
     async def update(item_id: int, item: request_schema):
         updated_item = await model.update_one(item_id, item)
         return await response_schema.from_queryset_single(updated_item)
